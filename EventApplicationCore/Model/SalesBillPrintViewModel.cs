f5f5b5c7ca1a87cc41d245ca332529817306cfe2
Srinivasan A J <ajsrinimca@gmail.com>
2020-01-01 21:17:58 +1100

    Dispatch Print

--- EventApplicationCore/Model/SalesBillPrintViewModel.cs
+++ EventApplicationCore/Model/SalesBillPrintViewModel.cs
@@ -5,7 +5,8 @@ namespace MyAccountProject.Model
     public class SalesBillPrintViewModel
     {
         public SalesBillViewModel SalesBillViewModel { get; set; }
-
         public List<SalesBillItemsViewModel> SalesItemBillViewModel { get; set; }
+        public List<string> PrintOptions { get; set; }
+        public bool DispatchPrint { get; set; }
     }
 }
